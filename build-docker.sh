2bd4c9106c67dc410510742f0545b6330ef0db3d
Jordan <solfonictoast@gmail.com>
2020-04-26 12:26:08 +0100

    feat: add docker build step for dashboards
    Signed-off-by: Jordan <solfonictoast@gmail.com>

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,3 @@
 docker build . -t datastream-hooks-topics:1.0.0 -f topics/Dockerfile
 docker build . -t datastream-hooks-connectors:1.0.0 -f connectors/Dockerfile
+docker build . -t datastream-hooks-dashboards:1.0.0 -f dashboards/Dockerfile
