aab77ce0d7686595c22218a46a9daf901c9ccd60
Jordan <solfonictoast@gmail.com>
2020-04-26 12:40:53 +0100

    fix: update script permissions
    Signed-off-by: Jordan <solfonictoast@gmail.com>

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
-docker build . -t datastream-hooks-topics:1.0.0 -f topics/Dockerfile
-docker build . -t datastream-hooks-connectors:1.0.0 -f connectors/Dockerfile
-docker build . -t datastream-hooks-dashboards:1.0.0 -f dashboards/Dockerfile
+docker build . -t jqrdan/datastream-hooks-topics:1.0.0 -f topics/Dockerfile
+docker build . -t jqrdan/datastream-hooks-connectors:1.0.0 -f connectors/Dockerfile
+docker build . -t jqrdan/datastream-hooks-dashboards:1.0.0 -f dashboards/Dockerfile
