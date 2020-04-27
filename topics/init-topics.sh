b3947e8c20afb9fc5a040feb3314f5b2cdb25510
Jordan <solfonictoast@gmail.com>
2020-04-27 17:20:48 +0100

    feat: update topics that are created
    Signed-off-by: Jordan <solfonictoast@gmail.com>

--- topics/init-topics.sh
+++ topics/init-topics.sh
@@ -17,11 +17,11 @@ echo "Creating topic genres"
 echo "Creating topic genre-averages"
 ./bin/kafka-topics.sh --create --zookeeper ZOOKEEPER --replication-factor 1 --partitions 3 --topic genre-averages
 
-echo "Creating topic song-averages"
-./bin/kafka-topics.sh --create --zookeeper ZOOKEEPER --replication-factor 1 --partitions 3 --topic song-averages
-
 echo "Creating topic artist-averages"
 ./bin/kafka-topics.sh --create --zookeeper ZOOKEEPER --replication-factor 1 --partitions 3 --topic artist-averages
+
+echo "Creating topic album-averages"
+./bin/kafka-topics.sh --create --zookeeper ZOOKEEPER --replication-factor 1 --partitions 3 --topic album-averages
 
 echo "Creating topic connect-configs"
 ./bin/kafka-topics.sh --create --zookeeper ZOOKEEPER --replication-factor 3 --partitions 1 --topic connect-configs
